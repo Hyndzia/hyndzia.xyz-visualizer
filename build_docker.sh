cb3a90df1967bd33584b13ba3167a489f99ccede
Hyndzia <118996952+Hyndzia@users.noreply.github.com>
2025-08-15 17:32:21 +0200

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Build the Docker image
-sudo docker build -t visualizer .
+sudo docker build -t visualizer visualizer
 
-# Run the Docker container mapping port 5000
-sudo docker run -p 8989:8989 -v /home/hyndzia/Visualizer/uploads:/uploads visualizer
+# Run the Docker container mapping your specific port
+sudo docker run -p 8989:8989 visualizer
 
 
