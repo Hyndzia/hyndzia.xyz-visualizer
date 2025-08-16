1dcfee3bc3ccb10023600fd73a1ecfc560cf338a
Hyndzia <118996952+Hyndzia@users.noreply.github.com>
2025-08-16 04:01:09 +0200

    Delete Visualizer directory

--- Visualizer/build_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-sudo docker build -t visualizer .
-
-# Run the Docker container mapping port 5000
-sudo docker run -p 8989:8989 -v /home/hyndzia/Visualizer/uploads:/uploads visualizer
-
-
